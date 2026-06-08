1588659c22774f7d151f922932c13fd5424e50d9
tuanttstb-debug <tuantt.stb@gmail.com>
2026-06-08 10:15:55 +0700

    config: update GS_WEBAPP_URL to new GAS deployment

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,4 +2,4 @@
 // Edit this file when redeploying the GAS Web App.
 // Do NOT put application logic here.
 
-const GS_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbzzezX0qvu73U7EBrsj7VeBoPbzg6edLNt818-pzlle2Gx2xfB-NQuxJYfx3jGHRcc/exec';
+const GS_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbyNWuk85hbwBYY2fI0kkIcEClQSuvjMz88zwme3sihjKBRyV7MMZ5d1EDG5LwxBKznN/exec';
